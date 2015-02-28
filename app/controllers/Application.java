289e8159dd7a41e0b7014d4b2942dc0cdd62930a
linhdragon <kawalinh@gmail.com>
2015-02-28 10:34:42 +0700

    add view for test how to remove compile error when add view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,22 +7,28 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Welcome Home"));
-    }
-    
-    public static Result hello(String name) {
-        return ok(index.render("Hello: "+name));
-    }
-    
-    public static Result sayhi(String name) {
-        return ok(index.render("Hello: "+name));
-    }
-    
-    public static Result sayhello(String name, int age) {
-    	
-//        return ok(index.render("Hello: "+name + " age: "+age));
-    	return ok(views.html.hello.render(name,age));
-    }
+	public static Result index() {
+		return ok(index.render("Welcome Home"));
+	}
+
+	public static Result hello(String name) {
+		return ok(index.render("Hello: " + name));
+	}
+
+	public static Result sayhi(String name) {
+		return ok(index.render("Hello: " + name));
+	}
+
+	public static Result sayhello(String name, int age) {
+		return ok(views.html.hello.render(name, age));
+	}
+	
+	public static Result addView() {
+		return ok(views.html.addView.render());
+	}
+	
+	public static Result view2() {
+		return ok(views.html.view2.render());
+	}
 
 }
